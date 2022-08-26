886ed68f18729ed09455040a780b135441ba831a
Logan McCalley <mccalleylogan@gmail.com>
2022-08-26 12:05:53 -0400

    connected to api

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react"
+
+export default function TodoList({tasklist, setTaskList}){
+    // call the api and use setTasklist to fill in state....
+    useEffect(() => {
+        fetch('https://three-do-api-lm.web.app/tasks')
+        .then(results => results.json())
+        .then(tasks => setTaskList(tasks))
+        .catch(console.error())
+        
+    }, [setTaskList])
+
+    if(!tasklist){
+        return <h2>No task to complete!</h2>
+      }
+    return(
+        <ul>
+            {tasklist.map(task => (
+                <li key={task.id}>{task.task}</li>
+            ))}
+        </ul>
+    )
+}
